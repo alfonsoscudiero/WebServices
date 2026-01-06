71e3313be9a168ca048020de0b6383e3b7068369
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-01-05 20:09:58 -0700

    Add Lesson 1 routes

---
+++ routes/lesson1.js
@@ -0,0 +1,22 @@
+/* ******************************************
+ * routes/lesson1.js
+ * ***************************************** */
+const express = require('express');
+const router = express.Router();
+// Import the controller 
+const lesson1Controller = require('../controllers/lesson1');
+
+// Route: GET /
+// http://localhost:3000/
+router.get('/', lesson1Controller.welcomeRoute);
+
+// Route: GET /student
+// http://localhost:3000/student
+router.get('/student', lesson1Controller.studentRoute);
+
+// Route: GET /hello
+// http://localhost:3000/hello
+router.get('/hello', lesson1Controller.helloRoute);
+
+// Export the router so server.js
+module.exports = router;
