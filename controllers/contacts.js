0173f866d17fae4a3817e0f80614d7ff36c12cdd
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-01-16 13:13:38 -0700

    Controller for GET a single contact document that matches the provided MongoDB _id

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,8 @@
 /* ***************************
  *  controllers/contacts.js
  * ************************** */
+// MongoDB ObjectId utility and database connection
+const { ObjectId } = require('mongodb');
 const { connectToDatabase } = require('../db/connection');
 
 // Controller for GET '/contacts'
@@ -22,6 +24,38 @@ const getContacts = async (req, res) => {
   }
 };
 
+// Controller for GET '/contacts/single?id=<mongoObjectId>'
+const getContactById = async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    if (!id) {
+      return res.status(400).json({ message: '[controllers/contacts] Missing id query parameter.'});
+    }
+
+    // Validate Mongo ObjectId format
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: '[controllers/contacts] Invalid id format.' });
+    }
+
+    const db = await connectToDatabase();
+
+    const contact = await db
+      .collection('contacts')
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!contact) {
+      return res.status(404).json({ message: '[controllers/contacts] Contact not found.' });
+    }
+
+    return res.status(200).json(contact);
+  } catch (error) {
+    console.error('[controllers/contacts] Error fetching contact by id:', error);
+    return res.status(500).json({ message: '[controllers/contacts] Server error.' });
+  }
+};
+
 module.exports = {
-  getContacts
+  getContacts,
+  getContactById
 };
